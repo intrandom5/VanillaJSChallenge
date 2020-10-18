8ef1f206b8eb1301e88b0d0529fc69857f49abc8
intrandom5 <68216991+intrandom5@users.noreply.github.com>
2020-10-18 13:33:05 +0900

    hide todo at initialize
    fix todoList has been shown before name has been inserted.

--- js/getName.js
+++ js/getName.js
@@ -1,14 +1,18 @@
 const initScreen = document.querySelector(".initialize");
 const mainScreen = document.querySelector(".header");
+const todo = document.querySelector(".todo");
+
 const getname = document.querySelector(".getName");
 const input= getname.querySelector("input");
 const greeting = document.querySelector(".greeting");
+
 
 const storeName = localStorage.getItem("username");
 
 function showContent(){
     initScreen.classList.remove("showing");
     mainScreen.classList.add("showing");
+    todo.classList.add("showing");
     greeting.innerText = `${storeName}님 반갑습니다!`
 }
 
